55557afe1c057953f77b5ab5f154feece1de32dd
Marcin <maruchiso.gasai@gmail.com>
2025-04-10 03:50:02 +0200

    10pkt

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from antlr.GeraltParser import GeraltParser
 from visitor import WitcherVisitor
 from ast_nodes import *
 from ir_generator import *
+from antlr4.error.ErrorListener import ErrorListener
 
 def print_ast(ast):
     for stmt in ast.statements:
@@ -19,21 +20,32 @@ def print_ast(ast):
         else:
             print(f"  Unknown node: {stmt}")
 
-            
+
+class GeraltErrorListener(ErrorListener):
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        raise SyntaxError(f"Syntax error at line {line}:{column} -> {msg}")
+
 def main():
     file_stream = FileStream('tests/program.witcher', encoding='utf-8')
+    
     lexer = GeraltLexer(file_stream)
-    token_stream = CommonTokenStream(lexer)
-    parser = GeraltParser(token_stream)
+    lexer.removeErrorListeners()
+    lexer.addErrorListener(GeraltErrorListener())  # Dodajemy nasłuchiwacz błędów
+
+    stream = CommonTokenStream(lexer)
+
+    parser = GeraltParser(stream)
+    parser.removeErrorListeners()
+    parser.addErrorListener(GeraltErrorListener())  # Dodajemy nasłuchiwacz błędów
+
     tree = parser.program()
 
     visitor = WitcherVisitor()
     ast = visitor.visit(tree)
-    #print_ast(ast)
-    
+
     ir_generator = IRGenerator()
     ir_generator.generate(ast)
-    ir_generator.save('output.ll')
+    ir_generator.save("output.ll")
     
 if __name__ == '__main__':
     main()
